25f676b3e992f143fa8f8a3043788cb6e997e3d7
William R <s80423@htw-dresden.de>
2024-02-22 13:06:11 +0100

    Update Stats: - print just from one year onwards - TH = Period of time

--- simulation/stats.py
+++ simulation/stats.py
@@ -13,24 +13,26 @@ def print_statistics(instance, days, dataset, disp, method='greedy', dir='greedy
     from instance import Instance
     instance: Instance
     lot: Lot
-    lots = defaultdict(lambda: {'ACT': [], 'throughput': 0, 'on_time': 0, 'tardiness': 0, 'waiting_time': 0,
+    lots = defaultdict(lambda: {'ACT': [], 'throughput_one_year': 0 , 'throughput': 0, 'on_time': 0, 'tardiness': 0, 'waiting_time': 0,
                                 'processing_time': 0, 'transport_time': 0, 'waiting_time_batching': 0})
     apt = {}
     dl = {}
     lot_list={}
     for lot in instance.done_lots:
-        lots[lot.name]['ACT'].append(lot.done_at - lot.release_at)
         lots[lot.name]['throughput'] += 1
-        lots[lot.name]['tardiness'] += max(0, lot.done_at - lot.deadline_at)
-        lots[lot.name]['waiting_time'] += lot.waiting_time
-        lots[lot.name]['waiting_time_batching'] += lot.waiting_time_batching
-        lots[lot.name]['processing_time'] += lot.processing_time
-        lots[lot.name]['transport_time'] += lot.transport_time
-        if lot.done_at <= lot.deadline_at:
-            lots[lot.name]['on_time'] += 1
-        if lot.name not in apt:
-            apt[lot.name] = sum([s.processing_time.avg() for s in lot.processed_steps])
-            dl[lot.name] = lot.deadline_at - lot.release_at
+        if lot.release_at >= 31536000:
+            lots[lot.name]['throughput_one_year'] += 1
+            lots[lot.name]['ACT'].append(lot.done_at - lot.release_at)
+            lots[lot.name]['tardiness'] += max(0, lot.done_at - lot.deadline_at)
+            lots[lot.name]['waiting_time'] += lot.waiting_time
+            lots[lot.name]['waiting_time_batching'] += lot.waiting_time_batching
+            lots[lot.name]['processing_time'] += lot.processing_time
+            lots[lot.name]['transport_time'] += lot.transport_time
+            if lot.done_at <= lot.deadline_at:
+                lots[lot.name]['on_time'] += 1
+            if lot.name not in apt:
+                apt[lot.name] = sum([s.processing_time.avg() for s in lot.processed_steps])
+                dl[lot.name] = lot.deadline_at - lot.release_at
     print('Lot', 'APT', 'DL', 'ACT', 'TH', 'ONTIME', 'tardiness', 'wa', 'pr', 'tr')
     acts = []
     ths = []
@@ -42,12 +44,12 @@ def print_statistics(instance, days, dataset, disp, method='greedy', dir='greedy
         acts += [avg]
         th = lots[lot_name]['throughput']
         ths += [th]
-        ontime = round(l['on_time'] / l['throughput'] * 100)
+        ontime = round(l['on_time'] / l['throughput_one_year'] * 100)
         ontimes += [ontime]
-        wa = lots[lot_name]['waiting_time'] / l['throughput'] / 3600 / 24
-        wab = lots[lot_name]['waiting_time_batching'] / l['throughput'] / 3600 / 24
-        pr = lots[lot_name]['processing_time'] / l['throughput'] / 3600 / 24
-        tr = lots[lot_name]['transport_time'] / l['throughput'] / 3600 / 24
+        wa = lots[lot_name]['waiting_time'] / l['throughput_one_year'] / 3600 / 24
+        wab = lots[lot_name]['waiting_time_batching'] / l['throughput_one_year'] / 3600 / 24
+        pr = lots[lot_name]['processing_time'] / l['throughput_one_year'] / 3600 / 24
+        tr = lots[lot_name]['transport_time'] / l['throughput_one_year'] / 3600 / 24
         print(lot_name, round(apt[lot_name] / 3600 / 24, 1), round(dl[lot_name] / 3600 / 24, 1), round(avg, 1), th,
               ontime, l['tardiness'], wa, wab, pr, tr)
     print('---------------')
